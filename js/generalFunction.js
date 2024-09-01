22cd36f52e76b591d86f4a285d088b06d278f150
Jonathan-Ultini <jonathanultini@gmail.com>
2024-09-01 11:46:47 +0200

    funzione verifica se un numero è primo

--- js/generalFunction.js
+++ js/generalFunction.js
@@ -46,3 +46,22 @@ function generateRandomNumber(min, max) {
 function isEven(number) {
   return number % 2 === 0;
 }
+
+
+/**
+ * Verifica se un numero è primo.
+ * 
+ * @param {number} number - Il numero da verificare.
+ * @returns {boolean} True se il numero è primo, false altrimenti.
+ */
+function isPrime(number) {
+  if (number <= 1) return false;
+  if (number === 2) return true;
+  if (number % 2 === 0) return false;
+
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    if (number % i === 0) return false;
+  }
+
+  return true;
+}
